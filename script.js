5142eeb9eb85ff20e0bc44f8ee3b84fe293d248c
malhar1271 <shirurmalhar@gmail.com>
2026-02-15 01:26:23 +0200

    Update image source to use dynamic file path

--- script.js
+++ script.js
@@ -102,7 +102,7 @@ for (let i = 0; i < totalFrames; i++) {
     frame.classList.add('frame');
 
     const img = document.createElement('img');
-img.src = "https://<malhar1271>.github.io/<Version-1>/images/Image-1.png";
+img.src = `images/${metadata[i].file}`;
 frame.appendChild(img);
     document.body.appendChild(frame);
 
