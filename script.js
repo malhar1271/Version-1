88432c04c1777f2682dfa204415195ddfc6fcf48
malhar1271 <shirurmalhar@gmail.com>
2026-02-15 01:30:29 +0200

    Update image source URL to use full path

--- script.js
+++ script.js
@@ -102,7 +102,7 @@ for (let i = 0; i < totalFrames; i++) {
     frame.classList.add('frame');
 
     const img = document.createElement('img');
-img.src = `images/${metadata[i].file}`;
+img.src = `https://malhar1271.github.io/Version-1/images/${metadata[i].file}`;
 frame.appendChild(img);
     document.body.appendChild(frame);
 
